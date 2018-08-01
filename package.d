fbe9a8ef7e65e2eb1379a348caf7c956767dd0b3
Matthias Rossmy <matti731140@gmx.net>
2018-08-01 17:55:32 +0200

    added string module

---
+++ package.d
@@ -0,0 +1,10 @@
+// for those people who are too lazy to write the import for each module ;-)
+
+module easyd;
+
+public import easyd.base;
+public import easyd.list;
+public import easyd.string;
+
+public import std.stdio;
+public import std.algorithm.searching;
---
+++ string.d
@@ -0,0 +1,159 @@
+module easyd.string;
+
+// (C) 2014-2018 by Matthias Rossmy
+// This file is distributed under the "Fair Use License v2"
+
+import std.conv;
+
+unittest
+{
+	string s = "Hello World!";
+	assert(s.countChar('l')==3);
+	assert(s.find('H')==0);
+	assert(s.find("World")==6);
+	assert(s.contains('!'));
+	assert(s.contains("or"));
+	assert(s.replace("l","L")=="HeLLo WorLd!");
+	assert(s.subStr(1,4)=="ello");
+	assert(s.getEnd(1)=="!");
+	assert(s.cutEnd(7)=="Hello");
+	assert(s.grow(20,'-',true)=="--------Hello World!");
+	int[] a = [1,2,3];
+	assert(a.concat(",")=="1,2,3");
+}
+
+size_t countChar(string s, char c)
+{
+    size_t result = 0;
+    foreach(x;s) if(x==c) result++;
+    return result;
+}
+
+long find(string s, char c, long start=0)
+{
+    for(long pos=start; pos<s.length.to!long; pos++)
+    {
+        if(s[pos]==c) return pos;
+    }
+    return -1;
+}
+
+bool contains(string s, char c, long start=0, long* foundAtPos=null)
+{
+    long p = find(s,c,start);
+    if(foundAtPos)
+    {
+        *foundAtPos = p;
+    }
+    return p>=0;
+}
+
+long find(string s, string f, long start=0, bool returnLengthIfNotFound=false)
+{
+	//writeln("Searching for "~f~" in "~s~" from position "~start.to!string);
+    long maxpos = s.length.to!long-f.length.to!long;
+    for(long pos=start; pos<=maxpos; pos++)
+    {
+        for(long offset=0; offset<f.length; offset++)
+        {
+            if(s[pos+offset]!=f[offset]) goto next;
+        }
+        return pos;
+        next:;
+    }
+	return returnLengthIfNotFound? s.length : -1;
+}
+
+bool contains(string s, string f, long start=0, long* foundAtPos=null)
+{
+    long p = find(s,f,start);
+    if(foundAtPos)
+    {
+        *foundAtPos = p;
+    }
+    return p>=0;
+}
+
+string replace(string s, string f, string r)
+{
+	//writeln("Replacing "~f~" with "~r~" in "~s);
+	long start=0;
+    long findpos;
+    while((findpos=s.find(f,start))>=0)
+    {
+        s = s[0..findpos] ~ r ~ s[findpos+f.length..s.length];
+        start = findpos+1+r.length.to!long-f.length.to!long;
+		if(start<=0) start = 1;
+    }
+    return s;
+}
+
+string subStr(string s, long start, long length=-1)
+{
+	if (start > s.length)
+	{
+		return "";
+	}
+	if (start<0)
+	{
+		start = 0;
+	}
+	long end;
+	if (length<0)
+	{
+		end = s.length;
+	}
+	else
+	{
+		end = start+length;
+	}
+	if(end>s.length)
+	{
+		end=s.length;
+	}
+	return s[start..end];
+}
+
+string cutEnd(string s, long count)
+{
+	if (s.length > count)
+	{
+		return s.subStr(0, s.length - count);
+	} 
+	else
+	{
+		return "";
+	}
+}
+
+string getEnd(string s, long count)
+{
+	return s.subStr(s.length - count, -1);
+}
+
+string grow(string s, int length, char fillChar=' ', bool atBeginning=false)
+{
+	if(s.length>=length) return s;
+	string fillString;
+	for(int x=0; x<(length-s.length); x++) fillString ~= fillChar;
+	if(atBeginning)
+	{
+		return fillString ~ s;
+	}
+	else
+	{
+		return s ~ fillString;
+	}
+}
+
+string concat(T)(T[] a,string separator="",string delegate(T) tostr = item=>item.to!string)
+{
+	string result;
+	for(int x=0; x<a.length; x++)
+	{
+		result ~= tostr(a[x]);
+		if(x<a.length-1) result ~= separator;
+	}
+	return result;
+}
+
